f1de98084eab8a575014f3c2938f4180707873b8
Om Nepal <nepalom15@gmail.com>
2025-08-07 17:51:21 -0500

    use fetch to send request to backend with the input url

--- frontend/bot-frontend/src/App.jsx
+++ frontend/bot-frontend/src/App.jsx
@@ -6,13 +6,34 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const url = e.target.url.value;
+    if (!url) {
+      console.log("Please enter valid URL");
+      return;
+    }
+
+    const response = await fetch('http://localhost:5000/summarize', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ url })
+    })
+
+    const data = await response.json();
+
+  }
+
   return (
     <div>
       <h2>Youtube Video Summary Bot</h2>
       <p>Summarize any youtube video. Just provide the link.</p>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <label htmlFor="">Paste the URL: </label>
-        <input type="text" name="url" id="url" />
+        <input type="text" name="url" id="urlInput" />
         <button type='submit'>Get Summary</button>
       </form>
     </div>
