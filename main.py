5b482124c941b09a56eb93875ee040846973af92
Om Nepal <nepalom15@gmail.com>
2025-08-07 15:53:04 -0500

    re setup everything and extract vide transcript using a python liibrary

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from youtube_transcript_api import YouTubeTranscriptApi
+
+ytt_api = YouTubeTranscriptApi()
+fetched_transcript = ytt_api.fetch('ZzI9JE0i6Lc')
+
+
+transcript = ''
+for snippet in fetched_transcript:
+  transcript += snippet.text + ' '
+
+print(transcript)
